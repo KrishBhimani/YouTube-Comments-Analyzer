56bcafcbb0bae90dfabca417c925d6afe07923c9
KrishBhimani <126689784+KrishBhimani@users.noreply.github.com>
2024-09-27 06:15:25 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# YouTube-Comments-Analyzer
+# Comment Analyzer
+To run this project must download pickle folder.
+Steps to run :
+1. clone the repository
+2. download the pickle folder
+3. copy or move the pickle folder inside the cloned repository
+4. install the libraries by command : pip install -r requirements.txt
+5. run the command : flask run --reload
+* *Note :* Don't forget to install required libraries
+
+* download the datasets : https://drive.google.com/drive/folders/1usfCyRQBis7hXXeNNdJeE46SemkYSEvg?usp=drive_link
+* download the pickle files : https://drive.google.com/drive/folders/1HyUhtNc_XhTfsXgJyGBvfaXdiEZUu8qO?usp=drive_link
+
+
+![Screenshot (11)](https://github.com/user-attachments/assets/a2810c45-0152-4abd-84aa-df9ec8bef2f4)
+
+![Screenshot (6)](https://github.com/user-attachments/assets/ea35f5bc-4b18-42d1-a198-e889d6adc8ff)
+
+
+![Screenshot (7)](https://github.com/user-attachments/assets/6ef3a6a6-146a-46b5-b73e-cef084e344f6)
---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, request
+# from sklearn.feature_extraction.text import CountVectorizer
+from preprocess import textPreprocess
+import pandas as pd
+import pickle
+import numpy as np
+
+sentimentAnalyzer = pickle.load(open('sentimentAnalyzer.pkl','rb'))
+spamDetector = pickle.load(open('spam_model_with_pipe.pkl','rb'))
+clusterModel = pickle.load(open('clusterModel.pkl','rb'))
+
+
+path = 'prediction.csv'
+df = pd.read_csv(path)
+
+app = Flask(__name__)
+class_senti = ['Irrelevant', 'Neutral', 'Negative', 'Positive']
+class_spam = ['No Spam', 'Spam']
+
+@app.route('/')
+def index():
+    return render_template('index.html', comments = df['comment'].values,
+                           spam = df['pred_spam'].values,
+                           senti = df['pred_sentiment'].values,
+                           class_spam = class_spam, class_senti = class_senti, cluster = df['cluster'].values,
+                           n = len(df['comment'].values))
+
+@app.route('/pred',methods=['POST', "GET"])
+def pred():
+    text = request.form.get('comment')
+    processedText = textPreprocess(text)
+    spam = spamDetector.predict(np.array([processedText])) 
+    spam = class_spam[spam[0]]
+
+    senti = sentimentAnalyzer.predict(np.array([processedText])) 
+    senti = class_senti[senti[0]]
+
+    cluster = clusterModel.predict([processedText])
+    return render_template('prediction.html',
+                           spam = spam, text = text, senti=senti, cluster=cluster)
+@app.route('/prediction')
+def prediction():
+    return render_template('prediction.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+if __name__=='__main__':
+    Flask.run(reload=True)
